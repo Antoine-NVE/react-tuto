79000c8d24afec94e627e92aa183fdcd6cbcdf62
Antoine-NVE <antoinenavette@gmail.com>
2025-05-15 21:05:55 +0200

    feat: first commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,38 @@
+const About = () => {
+    return (
+        <>
+            <h1>À propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quo facere qui, libero temporibus ipsam
+                quidem voluptate, id amet, exercitationem aperiam. Sint ad non veniam itaque amet molestias,
+                voluptatibus autem provident, repellendus iusto delectus nulla explicabo aliquam ratione aspernatur
+                quos, quaerat adipisci sapiente? Deleniti mollitia illo eveniet eius qui similique deserunt iste, et
+                nemo esse a exercitationem laboriosam voluptatum sit itaque. Sunt tempora, et nostrum a maxime id
+                maiores laborum placeat tenetur quo ipsum suscipit consequuntur veniam consectetur vero quasi sint
+                dolores quisquam laudantium harum odit, praesentium modi! Doloribus omnis eligendi fugit nisi!
+                Consectetur nesciunt molestiae hic quis quibusdam cumque.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore debitis adipisci distinctio architecto
+                fugit. Excepturi itaque reiciendis a aliquam ducimus commodi nemo ipsa molestiae necessitatibus repellat
+                sed illo incidunt quod suscipit non, fuga, tempore sequi! Dicta deserunt facere voluptas reprehenderit
+                explicabo praesentium tempore a recusandae porro velit incidunt quo nemo, doloribus soluta, minus earum
+                nobis quaerat maiores nostrum temporibus illo delectus consectetur. Autem maiores assumenda voluptate
+                ipsa sint corrupti placeat nisi magni! Libero ab amet, provident voluptate, magnam architecto labore
+                quibusdam modi quas non ipsa aliquid cupiditate soluta vel? Expedita neque cum impedit, delectus
+                possimus soluta mollitia quaerat exercitationem libero, eaque iusto inventore placeat asperiores sint
+                modi repudiandae suscipit atque at architecto consequuntur excepturi eligendi earum dolorem. Suscipit
+                provident quasi a ullam doloremque, tempora quia id delectus adipisci libero illum magnam inventore
+                earum vel sint mollitia possimus nisi voluptates eaque eius minus ratione eos ipsa! Soluta corporis
+                ullam, earum est placeat, quos numquam vitae ea autem illo optio! Voluptatem in ad officiis magni iste.
+                Quibusdam, sequi soluta dolores, totam numquam magnam libero quaerat iure ut cupiditate ducimus suscipit
+                corrupti saepe eum! Necessitatibus dolore fuga delectus quis, officiis incidunt, nostrum maxime,
+                repellat alias temporibus eum velit tenetur beatae neque blanditiis! Temporibus.
+            </p>
+        </>
+    );
+};
+
+export default About;
